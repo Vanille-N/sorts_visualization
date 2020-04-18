ca457a68740a86a9cd7472b372dcb71a36b5649b
Vanille-N <neven.villani@gmail.com>
2020-04-18 20:38:16 +0200

    Update to add shearsort

--- win32/moc_xslider.cpp
+++ win32/moc_xslider.cpp
@@ -7,7 +7,7 @@
 *****************************************************************************/
 
 #include <memory>
-#include "../Sorts/xslider.h"
+#include "../sorts/xslider.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
