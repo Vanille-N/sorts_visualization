a3f4352aa1b310314790f9647a87f961e8095939
Vanille-N <neven.villani@gmail.com>
2020-04-18 20:39:02 +0200

    Update to add shearsort

--- win32/moc_optionmgr.cpp
+++ win32/moc_optionmgr.cpp
@@ -7,7 +7,7 @@
 *****************************************************************************/
 
 #include <memory>
-#include "../Sorts/optionmgr.h"
+#include "../sorts/optionmgr.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
--- win32/moc_window.cpp
+++ win32/moc_window.cpp
@@ -7,7 +7,7 @@
 *****************************************************************************/
 
 #include <memory>
-#include "../Sorts/window.h"
+#include "../sorts/window.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
